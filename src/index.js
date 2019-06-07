cb1843193c105885aa14763dfccce3518d55fe88
Noah Hergenreder <noah.hergenreder@mutualofomaha.com>
2019-06-07 14:31:01 -0500

    added api js file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
+//This will be the actual API file
 const express = require('express');
-const bodyParser = require('body-parser');
 
+//Now set up the API
 const api = express();
-api.use(express.static(__dirname + '/public'));
-api.use(bodyParser.json());
-
 api.listen(3000, () => {
-  console.log('API up and running!');
+	console.log('API up and running!')
 });
 
-// api.get('/', (req, res) => {
-//  console.log(req);
-//  res.send('Hello, world!');
-//});
-
-api.post('/add', (req, res) => {
-	console.log(req.body);
-	res.send('It works!');
+//Now let's set up the routes
+api.get('/', (req, res) => {
+	console.log(req);
+	res.send('Hello, world!');
 });
